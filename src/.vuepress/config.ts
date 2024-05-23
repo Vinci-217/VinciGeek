005f64bad4116e53193c54d06c5c56c1189c3067
Vinci <1341060890@qq.com>
2024-05-23 22:01:21 +0800

    修改配置

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/Vinci/",
 
   lang: "zh-CN",
   title: "Vinci",
